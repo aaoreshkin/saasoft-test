4d85fd6917fc91b88ded3cba1254d3713511cbf4
Александр Орешкин <oreshkin.dev@outlook.com>
2025-06-09 23:13:48 +0300

    подготовка проекта

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import app from '@/App.vue'
 
-const app = createApp(App)
+const instance = createApp(app)
 
-app.use(createPinia())
+instance.use(createPinia())
 
-app.mount('#app')
+instance.mount('#app')
